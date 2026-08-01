49a07e8962ce20a89439fd0ae41bf37f9f895227
alhasan947 <251-15-005@diu.edu.bd>
2026-08-01 23:41:19 +0600

    Implement basic division with exception handling

---
+++ week-8/task-1/Solution.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        try {
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+            int result = x / y;
+            System.out.println(result);
+        } catch (InputMismatchException e) {
+            System.out.println("java.util.InputMismatchException");
+        } catch (ArithmeticException e) {
+            System.out.println("java.lang.ArithmeticException: / by zero");
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        sc.close();
+    }
+}
