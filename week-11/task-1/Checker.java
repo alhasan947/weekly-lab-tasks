8a0d6eaea3b5d3617ee122d41a85b2659436e3d9
alhasan947 <251-15-005@diu.edu.bd>
2026-08-01 23:58:46 +0600

    Implement Checker class for Player comparison

---
+++ week-11/task-1/Checker.java
@@ -0,0 +1,11 @@
+import java.util.Comparator;
+
+class Checker implements Comparator<Player> {
+    public int compare(Player a, Player b) {
+        if (a.score == b.score) {
+            return a.name.compareTo(b.name);
+        } else {
+            return b.score - a.score;
+        }
+    }
+}
